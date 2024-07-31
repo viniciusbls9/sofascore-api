3b8979a9339ec09b9099e6e497ffa2613919f2c6
Vinicius <beneditoloura@gmail.com>
2024-07-31 07:17:55 -0300

    feat: update user entity

--- internal/app/entity/entity.go
+++ internal/app/entity/entity.go
@@ -1,7 +1,14 @@
 package entity
 
+import "time"
+
 type User struct {
-	ID    int
-	Name  string
-	Email string
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	Velocity     int16     `json:"velocity"`
+	Fav_position string    `json:"fav_position"`
+	Rating       int16     `json:"rating"`
+	Biography    string    `json:"biography"`
+	Created_at   time.Time `json:"created_at"`
 }
