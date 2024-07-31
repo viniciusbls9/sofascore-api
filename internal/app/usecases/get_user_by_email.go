ec6bf5fe0859169a046f64f5023baf57bde4bcf0
Vinicius <beneditoloura@gmail.com>
2024-07-31 07:29:17 -0300

    feat: create get_user_by_email to help create user usecase

---
+++ internal/app/usecases/get_user_by_email.go
@@ -0,0 +1,29 @@
+package usecases
+
+import (
+	"fmt"
+
+	"github.com/viniciusbls9/sofascore-api/internal/app/entity"
+	"github.com/viniciusbls9/sofascore-api/internal/infrastructure/db"
+)
+
+func HandlerGetUserByEmail(email string) (*entity.User, error) {
+	db, err := db.HandlerOpenDatabaseConnection()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	var user entity.User
+	err = db.QueryRow("SELECT * FROM users WHERE email=$1", email).
+		Scan(&user.ID, &user.Name, &user.Email, &user.Velocity, &user.Fav_position, &user.Rating, &user.Biography, &user.Created_at)
+
+	if err != nil {
+		if err.Error() == "sql: no rows in result set" {
+			return nil, nil // No user found
+		}
+		return nil, fmt.Errorf("failed to query user: %v", err)
+	}
+
+	return &user, nil
+}
