40d7b3dccce0e1b4495603c5befb8b46f48689e5
Vinicius <beneditoloura@gmail.com>
2024-08-19 06:31:29 -0300

    refactor: change function handler

--- internal/app/usecases/handler_get_user_by_email.go
+++ internal/app/usecases/handler_get_user_by_email.go
@@ -1,29 +1,24 @@
 package usecases
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/viniciusbls9/sofascore-api/internal/app/entity"
-	"github.com/viniciusbls9/sofascore-api/internal/infrastructure/db"
+	"github.com/viniciusbls9/sofascore-api/internal/app/utils"
 )
 
-func HandlerGetUserByEmail(email string) (*entity.User, error) {
-	db, err := db.HandlerOpenDatabaseConnection()
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %v", err)
-	}
-	defer db.Close()
+func HandlerGetUserByEmail(w http.ResponseWriter, r *http.Request) {
+	userEmail := r.URL.Query().Get("email")
 
-	var user entity.User
-	err = db.QueryRow("SELECT * FROM users WHERE email=$1", email).
-		Scan(&user.ID, &user.Name, &user.Email, &user.Velocity, &user.Fav_position, &user.Rating, &user.Biography, &user.Created_at, &user.Image_url)
-
+	user, err := GetUserByEmail(userEmail)
 	if err != nil {
-		if err.Error() == "sql: no rows in result set" {
-			return nil, nil // No user found
-		}
-		return nil, fmt.Errorf("failed to query user: %v", err)
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	return &user, nil
+	if user == nil {
+		utils.RespondWithError(w, http.StatusNotFound, "User not found")
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, user)
 }
