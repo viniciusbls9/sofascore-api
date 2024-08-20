105e0f68ee8789f2650c34fcf4bea9b136ddeece
Vinicius <beneditoloura@gmail.com>
2024-08-20 08:04:34 -0300

    refactor: remove unnecesary file

--- internal/app/usecases/has_user_voted.go
+++
@@ -1,9 +0,0 @@
-package usecases
-
-import "database/sql"
-
-func hasUserVoted(db *sql.DB, voterID string, votedUserID string) (bool, error) {
-	var exists bool
-	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM votes WHERE voter_id = $1 AND voted_user_id = $2)", voterID, votedUserID).Scan(&exists)
-	return exists, err
-}
