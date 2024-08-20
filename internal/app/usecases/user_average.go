c42f5fe87fac303f2d4ac5177ccdabb44da2b2ca
Vinicius <beneditoloura@gmail.com>
2024-08-20 19:18:01 -0300

    feat: add getUserVote functions

--- internal/app/usecases/user_average.go
+++ internal/app/usecases/user_average.go
@@ -7,54 +7,59 @@ import (
 	"github.com/viniciusbls9/sofascore-api/internal/app/entity"
 )
 
-// Função para calcular a média dos votos e a média geral
 func getAverageVotes(db *sql.DB, userID string) (entity.AverageVotes, error) {
-	var averageVotes entity.AverageVotes
+	var (
+		totalShotVote     int
+		totalMarkingVote  int
+		totalQualityVote  int
+		totalVelocityVote int
+		totalVotes        int
+	)
 
-	rows, err := db.Query(`
+	err := db.QueryRow(`
 		SELECT
-			COALESCE(AVG(shot_vote), 0) AS average_shot,
-			COALESCE(AVG(marking_vote), 0) AS average_marking,
-			COALESCE(AVG(quality_vote), 0) AS average_quality,
-			COALESCE(AVG(velocity_vote), 0) AS average_velocity
+			COALESCE(SUM(shot_vote), 0) AS total_shot_vote,
+			COALESCE(SUM(marking_vote), 0) AS total_marking_vote,
+			COALESCE(SUM(quality_vote), 0) AS total_quality_vote,
+			COALESCE(SUM(velocity_vote), 0) AS total_velocity_vote,
+			COUNT(*) AS total_votes
 		FROM votes
 		WHERE voted_user_id = $1
-	`, userID)
+	`, userID).Scan(&totalShotVote, &totalMarkingVote, &totalQualityVote, &totalVelocityVote, &totalVotes)
+
 	if err != nil {
-		return averageVotes, fmt.Errorf("couldn't get average votes: %v", err)
+		return entity.AverageVotes{}, fmt.Errorf("couldn't get average votes: %v", err)
 	}
-	defer rows.Close()
 
-	if rows.Next() {
-		err := rows.Scan(&averageVotes.ShotVote, &averageVotes.MarkingVote, &averageVotes.QualityVote, &averageVotes.VelocityVote)
-		if err != nil {
-			return averageVotes, fmt.Errorf("couldn't scan average votes: %v", err)
-		}
+	var averageVotes entity.AverageVotes
 
-		totalVotes := 0
-		totalSum := 0.0
-
-		if averageVotes.ShotVote > 0 {
-			totalVotes++
-			totalSum += averageVotes.ShotVote
-		}
-		if averageVotes.MarkingVote > 0 {
-			totalVotes++
-			totalSum += averageVotes.MarkingVote
-		}
-		if averageVotes.QualityVote > 0 {
-			totalVotes++
-			totalSum += averageVotes.QualityVote
-		}
-		if averageVotes.VelocityVote > 0 {
-			totalVotes++
-			totalSum += averageVotes.VelocityVote
-		}
-
-		if totalVotes > 0 {
-			averageVotes.OverallAverage = totalSum / float64(totalVotes)
-		}
+	if totalVotes > 0 {
+		averageVotes.ShotVote = float64(totalShotVote) / float64(totalVotes)
+		averageVotes.MarkingVote = float64(totalMarkingVote) / float64(totalVotes)
+		averageVotes.QualityVote = float64(totalQualityVote) / float64(totalVotes)
+		averageVotes.VelocityVote = float64(totalVelocityVote) / float64(totalVotes)
+		averageVotes.OverallAverage = (averageVotes.ShotVote + averageVotes.MarkingVote + averageVotes.QualityVote + averageVotes.VelocityVote) / 4
 	}
 
 	return averageVotes, nil
+}
+
+// getUserVote retorna a nota dada por um usuário específico para um usuário alvo
+func getUserVote(db *sql.DB, voterID string, votedUserID string) (entity.AverageVotes, error) {
+	var vote entity.AverageVotes
+
+	err := db.QueryRow(`
+		SELECT pass_vote, shot_vote, marking_vote, quality_vote, velocity_vote
+		FROM votes
+		WHERE voter_id = $1 AND voted_user_id = $2
+	`, voterID, votedUserID).Scan(&vote.PassVote, &vote.ShotVote, &vote.MarkingVote, &vote.QualityVote, &vote.VelocityVote)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return vote, nil
+		}
+		return vote, fmt.Errorf("couldn't get user vote: %v", err)
+	}
+
+	return vote, nil
 }
